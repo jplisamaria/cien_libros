4b205e3fedcb0c2675895be2a7b073388c95f6e8
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-05-01 13:39:44 -0400

    Updated to v1.9 hash syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 Book.destroy_all
 open('data/test_books.csv') do |books|
   books.read.each_line do |book|
-    title, author = book.chomp.split(",")
-    Book.create!(:title => title, :author => author)
+    title, author = book.chomp.split(',')
+    Book.create!(title: title, author: author)
   end
 end
-
