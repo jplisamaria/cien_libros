835d730e3733ad9997db7cdd5821d7b7857a1bc0
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-05-01 11:30:08 -0400

    Add Book model
    Seed database with sample book data
    
    Add null:false and validations to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430180710) do
+ActiveRecord::Schema.define(version: 20140501145826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title",      null: false
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
