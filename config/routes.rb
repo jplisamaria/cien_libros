6a0691f2d17d35a9148d6e8a8861c8ce4f822d89
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-05-10 21:22:04 -0400

    delete children related files

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,11 @@ Rails.application.routes.draw do
     resources :parent_profiles, only: [:edit, :update]
   end
 
-  # Research: should children and student profile be member routes?
   resources :parent_profiles, only: [:show] do
-    resources :children, only: [:new, :create]
     resources :student_profiles, only: [:new, :create]
   end
 
   resources :student_books, only: [:new, :create, :index]
   resources :books, only: [:create]
   resources :readings, only: [:index, :create]
-
 end
