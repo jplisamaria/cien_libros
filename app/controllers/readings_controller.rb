4b190501ee05ab596e4e8006e130e4edf11de679
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-05-12 23:45:03 -0400

    Clean-up readings_controller

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -8,11 +8,8 @@ class ReadingsController < ApplicationController
 
   def create
     title = params[:reading][:title]
-    book = Book.find_by(title: title)
-    student_book_id = StudentBook.find_by(
-      book_id: book.id,
-      student_profile_id: current_profile.id
-    ).id
+    @book = Book.find_by(title: title)
+    student_book_id = find_student_book_id
     Reading.create(reading_params.merge(student_book_id: student_book_id))
     redirect_to readings_path
   end
@@ -21,5 +18,12 @@ class ReadingsController < ApplicationController
 
   def reading_params
     params.require(:reading).permit(:amount_read)
+  end
+
+  def find_student_book_id
+    StudentBook.find_by(
+      book_id: @book.id,
+      student_profile_id: current_profile.id
+    ).id
   end
 end
