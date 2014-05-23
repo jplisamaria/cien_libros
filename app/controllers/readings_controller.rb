1491f9aedbdf3db3f59aef714d31b8a41772e9ba
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-05-23 15:15:41 -0400

    Update/Delete parents, students, and readings
    Fix readings display bug, and add delete students
    
    Update book with no author to book with author

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,27 +1,52 @@
 class ReadingsController < ApplicationController
   def index
-    @book = Book.new
+    @book ||= get_new_book
     @reading = Reading.new
-    @student = current_profile
-    @readings = @student.readings.newest_first
+    @readings = current_profile.readings.newest_first
+    @reading_to_be_updated ||= find_reading_to_be_updated
   end
 
   def create
-    title = params[:reading][:title]
-    book = Book.find_by(title: title)
-    student_book_id = find_student_book_id(book)
+    student_book_id = find_student_book_id
     Reading.create(reading_params.merge(student_book_id: student_book_id))
+    redirect_to readings_path
+  end
+
+  def update
+    reading = Reading.find(params[:id])
+    student_book_id = find_student_book_id
+    reading.update(reading_params.merge(student_book_id: student_book_id))
+    redirect_to readings_path
+  end
+
+  def destroy
+    reading = Reading.find(params[:id])
+    reading.destroy
     redirect_to readings_path
   end
 
   private
 
+  def find_reading_to_be_updated
+    if params[:readings_id]
+      Reading.find(params[:readings_id])
+    end
+  end
+
   def reading_params
     params.require(:reading).permit(:amount_read)
   end
 
-  def find_student_book_id(book)
-    StudentBook.find_by(
+  def get_new_book
+    if params[:new_book]
+      Book.new
+    end
+  end
+
+  def find_student_book_id
+    title = params[:reading][:title]
+    book = Book.find_by(title: title)
+    StudentBook.find_or_create_by(
       book_id: book.id,
       student_profile_id: current_profile.id
     ).id
