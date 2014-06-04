fe792f4dd798e73441bc37401658638da19b066a
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-06-03 22:02:43 -0400

    Real Final

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -8,9 +8,9 @@ class TeacherProfilesController < ApplicationController
   private
 
   def students_by_teacher
-    teacher_student_relationships.map {
-      |relationship| find_student_profile(relationship.student_profile_id)
-    }
+    teacher_student_relationships.map do |relationship| 
+      find_student_profile(relationship.student_profile_id)
+    end
   end
 
   def teacher_student_relationships
